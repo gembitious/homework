c34b40c6c5084e0f6e47ebf8ca6bbe6c318e9fe9
gembitious <hassamnet@gmail.com>
2020-10-15 21:13:25 +0900

    test2
    test2

--- hw03.py
+++ hw03.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713',headers=headers)
@@ -13,3 +17,9 @@ for music in musicList:
     music_name = music.select_one('td.info > a.title.ellipsis').text.strip()
     artist = music.select_one('td.info > a.artist.ellipsis').text.strip()
     print(rank, music_name, artist)
+    doc = {
+        'rank': rank,
+        'title': music_name,
+        'artist': artist
+    }
+    db.musicchart.insert_one(doc)
